52be4622570b889d6ca67cfdc344e8d066398ed6
AdiDD <adi.deaconu21@gmail.com>
2020-05-09 03:19:46 +0300

    Setup MongoDB config files

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB database");
+  } catch (err) {
+    console.error(err);
+    // Exit process in case of failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
