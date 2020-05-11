e62b16157fcf6214b3853bdccd569b1475eb3c1f
AdiDD <adi.deaconu21@gmail.com>
2020-05-11 15:00:40 +0300

    Created Login and Register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar.component";
 import Landing from "./components/layout/Landing.component";
+import Login from "./components/auth/Login.component";
+import Register from "./components/auth/Register.component";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Landing />
-    </>
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Router>
   );
 };
 
