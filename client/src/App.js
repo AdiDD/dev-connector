04e43383ef482db72ec6f94c430713735e1e653d
AdiDD <adi.deaconu21@gmail.com>
2020-05-11 13:41:43 +0300

    Created Navbar and Landing(page) component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+
+import Navbar from "./components/layout/Navbar.component";
+import Landing from "./components/layout/Landing.component";
+
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <h1>App</h1>
+      <Navbar />
+      <Landing />
     </>
   );
 };
