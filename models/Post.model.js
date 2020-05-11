e13f811dfb9ff422addfc2582d14e75fbc1e5e80
AdiDD <adi.deaconu21@gmail.com>
2020-05-11 08:17:01 +0300

    Fixed user type mistake

--- models/Post.model.js
+++ models/Post.model.js
@@ -18,7 +18,7 @@ const PostSchema = new mongoose.Schema({
   likes: [
     {
       user: {
-        type: Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "users",
       },
     },
@@ -26,7 +26,7 @@ const PostSchema = new mongoose.Schema({
   comments: [
     {
       user: {
-        type: Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "users",
       },
       text: {
