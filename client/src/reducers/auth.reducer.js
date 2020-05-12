ec2a78d1a450130f76c751024e5257e208426f19
AdiDD <adi.deaconu21@gmail.com>
2020-05-13 02:57:45 +0300

    Implemented logout functionality

--- client/src/reducers/auth.reducer.js
+++ client/src/reducers/auth.reducer.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from "../actions/types";
 
 const INITIAL_STATE = {
@@ -35,6 +36,7 @@ export default function (state = INITIAL_STATE, action) {
     case AUTH_ERROR:
     case REGISTER_FAIL:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token");
       return {
         ...state,
