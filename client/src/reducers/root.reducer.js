2fa4274e073ab825c080de16031b42a846481b2a
AdiDD <adi.deaconu21@gmail.com>
2020-05-13 03:58:57 +0300

    Created profile reducer

--- client/src/reducers/root.reducer.js
+++ client/src/reducers/root.reducer.js
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
 import alertReducer from "./alert.reducer";
 import authReducer from "./auth.reducer";
+import profileReducer from "./profile.reducer";
 
-const rootReducer = combineReducers({ alertReducer, authReducer });
+const rootReducer = combineReducers({
+  alertReducer,
+  authReducer,
+  profileReducer,
+});
 
 export default rootReducer;
