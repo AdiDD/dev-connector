5b821a79e76f12b4132de1ae5a204e51f922ef0e
AdiDD <adi.deaconu21@gmail.com>
2020-05-22 20:29:15 +0300

    Created PostItem component

--- client/src/components/posts/Posts.component.js
+++ client/src/components/posts/Posts.component.js
@@ -2,25 +2,43 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import { getPosts } from "../../actions/post.actions";
 import Spinner from "../layout/Spinner.component";
+import PostItem from "./PostItem.component";
 
-const Posts = ({ getPosts, user, post: { posts, loading } }) => {
+import { getPosts } from "../../actions/post.actions";
+
+const Posts = ({ getPosts, auth: { user }, post: { posts, loading } }) => {
   useEffect(() => {
     if (user) getPosts();
   }, [user, getPosts]);
 
-  return <div></div>;
+  return loading || !user ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome to the comunity
+      </p>
+      {/* PostForm */}
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 Posts.propTypes = {
   getPosts: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   post: state.postReducer,
-  user: state.authReducer.user,
+  auth: state.authReducer,
 });
 
 export default connect(mapStateToProps, { getPosts })(Posts);
